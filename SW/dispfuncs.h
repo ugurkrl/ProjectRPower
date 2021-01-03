d6d861dade3a67f1e17435a8a9ad1552b8d60605
ugurkrl <ugurkrl@outlook.com>
2021-01-04 01:50:13 +0300

    SW:DISP:Remove uImageDisp Function

--- SW/dispfuncs.h
+++ SW/dispfuncs.h
@@ -9,18 +9,5 @@
 /* Function for drawing images to OLED from array.Inside array 0 means clear pixel , 1 means draw pixel and 2 means skip that pixel
  *  I'll try to use progmem later. For now this will enough for drawing charging icon
  */
-void drawuImage(int x,int y,int width,int height,byte arr[]){
-  int loc = 0;
-  for(int m=0; m < height ; m++){;
-     for(int n=0; n < width ; n++){
-      if(arr[loc]!=2){
-           if(arr[loc]==0){ oled.setDrawColor(0);}
-           if(arr[loc]==1){ oled.setDrawColor(1);}
-           oled.drawPixel(x+n,y+m);
-       }
-      loc = loc +1;
-    }
-  }
-  oled.setCursor(0,0);
-}
+
   
